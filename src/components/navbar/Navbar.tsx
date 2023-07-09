e33405d926c7d644a0b47671d6adb583f190c73a
Oscar Estrada <arturostrada2020@gmail.com>
2023-07-09 13:36:36 -0600

    refactor(ui): create array links

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,13 +1,29 @@
 import { Link } from "react-router-dom";
 
+const links = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    title: "TicTacToe",
+    path: "/tic-tac-toe",
+  },
+];
+
 const Navbar = () => {
   return (
     <header className="w-full px-10 py-5">
       <nav className="flex items-center justify-between">
         <span className="text-xl font-semibold">Logo</span>
         <ul className="flex gap-5">
-          <Link to="/">Home</Link>
-          <Link to="/tic-tac-toe">TicTacToe</Link>
+          {links.map((link) => (
+            <Link className="hover:text-blue-400" key={link.id} to={link.path}>
+              {link.title}
+            </Link>
+          ))}
         </ul>
       </nav>
     </header>

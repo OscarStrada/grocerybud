a5174e7f13aa4d6d96df96f0b9fe2ce89e0500e6
Oscar Estrada <arturostrada2020@gmail.com>
2023-07-09 12:55:31 -0600

    feat(router): add router to entry point app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import Root from "./routes/root.tsx";
+import ErrorPage from "./error-page.tsx";
+import TicTacToe from "./TicTacToe.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "tic-tac-toe",
+        element: <TicTacToe />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);

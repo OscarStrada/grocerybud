71274a5f4079f4d06d2451fb41a96a33127a80a2
Oscar Estrada <arturostrada2020@gmail.com>
2023-07-09 12:52:06 -0600

    pages: create TicTacToe page

---
+++ src/TicTacToe.tsx
@@ -0,0 +1,9 @@
+const TicTacToe = () => {
+  return (
+    <div className="text-center text-5xl">
+      <h1 className="font-bold text-blue-500">Tic Tac Toe</h1>
+    </div>
+  );
+};
+
+export default TicTacToe;

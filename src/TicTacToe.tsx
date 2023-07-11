9ae4b49f81516c160d50f0be3ee47c2728d8e4d7
Oscar Estrada <arturostrada2020@gmail.com>
2023-07-10 22:57:19 -0600

    feat(ui): add Square to TicTacToe page

--- src/TicTacToe.tsx
+++ src/TicTacToe.tsx
@@ -1,7 +1,12 @@
+import { Square } from "./components";
+
 const TicTacToe = () => {
   return (
     <div className="text-center text-5xl">
       <h1 className="font-bold text-blue-500">Tic Tac Toe</h1>
+      <div>
+        <Square />
+      </div>
     </div>
   );
 };
